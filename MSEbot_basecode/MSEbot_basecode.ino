5c6d3813795276af2fcb370f6c5eb5670dfb6138
Michael Naish <mnaish@uwo.ca>
2021-02-01 11:30:13 -0500

    Updated pin assignment table.

--- MSEbot_basecode/MSEbot_basecode.ino
+++ MSEbot_basecode/MSEbot_basecode.ino
@@ -5,18 +5,18 @@
 
 
 /*
-  esp32                                             MSE-DuinoV2
-  pins         description                        Brd Jumpers /Labels                                                                      User (Fill in chart with user PIN usage) 
-  1             3v3                               PWR 3V3                                                                                  3V3
-  2             gnd                               GND                                                                                      GND
+  esp32                                           MSE-DuinoV2
+  pins         description                        Brd Jumpers /Labels                                                                  User (Fill in chart with user PIN usage) 
+  1             3v3                               PWR 3V3                                                                              3V3
+  2             gnd                               GND                                                                                  GND
   3             GPIO15/AD2_3/T3/SD_CMD/           D15 (has connections in both 5V and 3V areas)                    
-  4             GPIO2/AD2_2/T2/SD_D0              D2(has connections in both 5V and 3V areas)  /INDICATORLED ( On ESP32 board
-  5             GPIO4/AD2_0/T0/SD_D1              D4(has connections in both 5V and 3V areas)  
-  6             GPIO16/RX2                        Slide Switch S1b
-  7             GPIO17/TX2                        Slide Switch S2b
-  8             GPIO5                             D5 (has connections in both 5V and 3V areas)  
-  9             GPIO18                            D18 (has connections in both 5V and 3V areas)  
-  10            GPIO19/CTS0                       D19 (has connections in both 5V and 3V areas)  
+  4             GPIO2/AD2_2/T2/SD_D0              D2(has connections in both 5V and 3V areas)  /INDICATORLED ( On ESP32 board )        Heartbeat LED
+  5             GPIO4/AD2_0/T0/SD_D1              D4(has connections in both 5V and 3V areas)                                          Left Motor, Channel A
+  6             GPIO16/RX2                        Slide Switch S1b                                                                     IR Receiver
+  7             GPIO17/TX2                        Slide Switch S2b                                                                     Left Encoder, Channel A
+  8             GPIO5                             D5 (has connections in both 5V and 3V areas)                                         Left Encoder, Channel B
+  9             GPIO18                            D18 (has connections in both 5V and 3V areas)                                        Left Motor, Channel B
+  10            GPIO19/CTS0                       D19 (has connections in both 5V and 3V areas)                                        Right Motor, Channel A
   11            GPIO21                            D21/I2C_DA  
   12            GPIO3/RX0                         RX0
   13            GPIO1//TX0                        TX0
@@ -27,14 +27,14 @@
   18            GPI39/VN/AD1_3/                   AD3
   19            GPI34/AD1_6/                      AD6
   20            GPI35/AD1_7                       Potentiometer R2 / AD7
-  21            GPIO32/AD1_4/T9                   Potentiometer R1 / AD4
+  21            GPIO32/AD1_4/T9                   Potentiometer R1 / AD4                                                               Pot 1 (R1)
   22            GPIO33/AD1_5/T8                   IMon/D33  monitor board current
-  23            GPIO25/AD2_8/DAC1                 SK6812 Smart LEDs / D25
-  24            GPIO26/A2_9/DAC2                  Push Button PB2
-  25            GPIO27/AD2_7/T7                   Push Button PB1
-  26            GPOP14/AD2_6/T6/SD_CLK            Slide Switch S2a
-  27            GPIO12/AD2_5/T5/SD_D2/            D12(has connections in both 5V and 3V areas)  
-  28            GPIO13/AD2_4/T4/SD_D3/            Slide Switch S1a
+  23            GPIO25/AD2_8/DAC1                 SK6812 Smart LEDs / D25                                                              Limit switch
+  24            GPIO26/A2_9/DAC2                  Push Button PB2                                                                      PB2
+  25            GPIO27/AD2_7/T7                   Push Button PB1                                                                      PB1
+  26            GPOP14/AD2_6/T6/SD_CLK            Slide Switch S2a                                                                     Right Encoder, Channel A
+  27            GPIO12/AD2_5/T5/SD_D2/            D12(has connections in both 5V and 3V areas)                                         Right Motor, Channel B
+  28            GPIO13/AD2_4/T4/SD_D3/            Slide Switch S1a                                                                     Right Encoder, Channel B
   29            GND                               GND                                                                                  GND
   30            VIN                               PWR 5V t 7V                                                                          PWR 5V to 7V
 
@@ -82,8 +82,6 @@ uint32_t CR1_u32Now;
 uint32_t CR1_u32Last;
 uint32_t CR1_u32Temp;
 uint32_t CR1_u32Avg;
-
-
 
 unsigned long CR1_ulLastDebounceTime;
 
